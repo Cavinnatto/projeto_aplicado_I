52f2c2b537a8cb98e06d17cdf49965bb306771bb
Cavinnatto <andrec.inkasa@gmail.com>
2025-09-11 09:22:55 -0300

    Update 01_carregamento_dados.py

--- 01_carregamento_dados.py
+++ 01_carregamento_dados.py
@@ -1,14 +1,11 @@
 import pandas as pd
 
-# Carregar datasets principais
 orders = pd.read_csv("data/olist_dataset/olist_orders_dataset.csv")
 reviews = pd.read_csv("data/olist_dataset/olist_order_reviews_dataset.csv")
 payments = pd.read_csv("data/olist_dataset/olist_order_payments_dataset.csv")
 
-# Exibir primeiras linhas
 print(orders.head())
 print(reviews.head())
 print(payments.head())
 
-# Estatísticas iniciais
 print(reviews['review_score'].describe())
